8ef6b9bc9f19d18aa3589b49917256e04e366486
lermadior60-max <lermadior60@gmail.com>
2026-06-07 01:26:09 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function mostraSeccion(idSeccion)
- {
- const secciones = document.querySelectorAll( '.seccion-contenido');
-
- secciones.forEach(sec => {
-    sec.style.display = 'none';   
- });
-document.getElementById(idSeccion).style.display = 'block';
-}
